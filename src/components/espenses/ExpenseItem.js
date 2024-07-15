053be68f56d3775de00dd8118e9a543e9799cd6a
tien <tienvn998@gmail.com>
2024-07-15 08:41:03 +0700

    insert state

--- src/components/espenses/ExpenseItem.js
+++ src/components/espenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 const name = new Date();
 
 function ExpenseItem({ exp }) {
+  const [title, setTitle] = useState(exp.title);
+  function clickedHandler() {
+    setTitle('updated!');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       {/* <div>{exp.date.toISOString()}</div> */}
       <ExpenseDate date={exp.date} />
       <div className="expense-item__description">
-        <h2>{exp.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${exp.amount}</div>
       </div>
+      <button onClick={clickedHandler}>Change Title</button>
     </Card>
   );
 }
