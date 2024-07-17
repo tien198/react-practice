dde26768ae7a692966eb1e9a9b3d92039f20c9e2
tien <tienvn998@gmail.com>
2024-07-17 15:17:35 +0700

    outputting conditional content

--- src/components/Espenses/ExpenseFilter.jsx
+++ src/components/Espenses/ExpenseFilter.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './ExpenseFilter.css';
 
-const ExpensesFilter = ({ filter, onFilterChange }) => {
-    function dropdownChangeHandler(e) {
+function ExpensesFilter({ filter, onFilterChange }) {
+    function filterChange(e) {
         onFilterChange(e.target.value);
     }
 
@@ -11,7 +11,7 @@ const ExpensesFilter = ({ filter, onFilterChange }) => {
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select value={filter} onChange={dropdownChangeHandler}>
+                <select value={filter} onChange={filterChange}>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
--- src/components/Espenses/Expenses.jsx
+++ src/components/Espenses/Expenses.jsx
@@ -3,28 +3,35 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card'
 import ExpenseFilter from "./ExpenseFilter";
 import './Expenses.css';
-
+const par = {
+    items: [{
+        title: '', amount: '', date: new Date()
+    }]
+}
+/**
+ * @param {par}  - description
+ */
 function Expenses({ items }) {
     const [filter, setFilter] = useState('2020');
     function filterChange(filterVal) {
         setFilter(filterVal);
     }
-
+    const filterVals = items.filter(e => e.date.getFullYear() === Number(filter));
     // const displayItems = [];
     // items.forEach(e => {
     // displayItems.push(<ExpenseItem exp={e} />)
     // });
     // console.log(items);
 
+    let filterContents = <p>No expense found</p>;
+    if (filterVals.length > 0)
+        filterContents = filterVals.map(e => <ExpenseItem exp={e} key={e.id} />)
+
     return (
         <div>
             <ExpenseFilter filter={filter} onFilterChange={filterChange} />
             <Card className='expenses'>
-                {items.map(e => <ExpenseItem exp={e} />)}
-                {/* <ExpenseItem exp={items[0]} /> */}
-                {/* <ExpenseItem exp={items[1]} /> */}
-                {/* <ExpenseItem exp={items[2]} /> */}
-                {/* <ExpenseItem exp={items[3]} /> */}
+                {filterContents}
             </Card>
         </div>
     );
